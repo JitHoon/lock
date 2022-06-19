1ba75140df0f4887df0d5e9311d2b594f7f4546e
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-06-19 22:36:47 +0900

    06.09.first

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🫡`);
+
+app.listen(PORT, handleListening);
