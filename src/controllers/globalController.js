19db846a62ac24c721fc05a5757adf6b5292715f
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-07-18 16:03:12 +0900

    07.18.afternoon pug master

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,4 +1,4 @@
-export const home = (req, res) => res.render("home");
+export const home = (req, res) => res.render("home", {pageTitle : "Home"});
 export const join = (req, res) => res.send("Join");
 export const login = (req, res) => res.send("Log in");
 export const findpw = (req, res) => res.send("Find Password");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { home, join, login, findpw } from "../controllers/homeController";
+import { home, join, login, findpw } from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
