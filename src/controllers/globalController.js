16cd416b866f23890e52930cc0458ce8ecc20b67
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-07-19 10:36:19 +0900

    07.19.morning sever.js에 lockerRouter 추가

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,4 +1,13 @@
-export const home = (req, res) => res.render("home", {pageTitle : "Home"});
+let students = [
+    {number : 12, available : Y, id : 1},
+    {number : 30, available : N, id : 2},
+    {number : 41, available : Y, id : 3}
+]
+
+export const home = (req, res) => {
+    return res.render("home", {pageTitle : "Home"});
+}
+
 export const join = (req, res) => res.send("Join");
 export const login = (req, res) => res.send("Log in");
 export const findpw = (req, res) => res.send("Find Password");
--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import morgan from "morgan";
 
 import globalRouter from "./routers/globalRouter";
+import lockerRouter from "./routers/lockerRouter";
 
 const app = express();
 const PORT = 4000;
 
+/*
 console.log(process.cwd());
+*/
+
 /*
 const logger = (req, res, next) => {
     console.log(`${req.method} ${req.url}`);
@@ -36,12 +40,14 @@ app.use(privativeMiddleware);
 app.get("/", handleHome);
 app.get("/protected", handleProtected);
 */
+
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(morgan("dev"));
 
 app.use("/", globalRouter);
+app.use("/locker", lockerRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🫡`);
 app.listen(PORT, handleListening);
