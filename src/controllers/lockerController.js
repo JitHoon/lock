c187cb444c05261a26b0123f53739ccc546195e4
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-07-26 14:42:19 +0900

    07.26.afternoon seeLocker 편집

--- src/controllers/lockerController.js
+++ src/controllers/lockerController.js
@@ -1,7 +1,8 @@
+// array DB
 let lockers = [
-    {number : 1, available : "Y", id : 1},
-    {number : 2, available : "N", id : 2},
-    {number : 3, available : "Y", id : 3}
+    {number : 1, available : true, id : 1},
+    {number : 2, available : false, id : 2},
+    {number : 3, available : true, id : 3}
 ];
 
 export const locker = (req, res) => {
@@ -12,5 +13,5 @@ export const seeLocker = (req, res) => {
     const { id } = req.params;
     const locker = lockers[id-1];
 
-    return res.render("seeLocker", {pageTitle : `No. ${locker.number} Lockers`});
+    return res.render("seeLocker", {pageTitle : `No. ${locker.number} Lockers`, locker});
 }
