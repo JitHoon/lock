8f8ed2e56fa415cfc4994f884d6834aae42f9291
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-08-08 18:50:55 +0900

    08.08.afternoon connect MongoDb

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/locker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("❌ DB Error", error);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
