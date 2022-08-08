5ece4c244698c2b27e160542628f3798d5a5d58d
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-08-08 16:02:40 +0900

    08.08.afternoon route get, post 표기방식 변경

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -3,8 +3,8 @@ import { home, qna, getQ, postQ } from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", home);
-globalRouter.get("/qna", qna);
+globalRouter.route("/").get(home);
+globalRouter.route("/qna").get(qna);
 globalRouter.route("/question").get(getQ).post(postQ);
 
 export default globalRouter;
--- src/routers/lockerRouter.js
+++ src/routers/lockerRouter.js
@@ -3,8 +3,8 @@ import { locker, seeLocker, seePassword } from "../controllers/lockerController"
 
 const lockerRouter = express.Router();
 
-lockerRouter.get("/", locker);
-lockerRouter.get("/:id(\\d+)", seeLocker);
-lockerRouter.get("/:id(\\d+)/password", seePassword);
+lockerRouter.route("/").get(locker);
+lockerRouter.route("/:id(\\d+)").get(seeLocker);
+lockerRouter.route("/:id(\\d+)/password").get(seePassword);
 
 export default lockerRouter;
