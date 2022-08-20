c5ee74472562477238e639736c20d342531c3e2e
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-08-20 20:23:06 +0900

    08.20.night create qnaRouter

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -4,9 +4,5 @@ import { home, qna, seeQ, getUploadQ, postUploadQ, getEditQ, postEditQ } from ".
 const globalRouter = express.Router();
 
 globalRouter.route("/").get(home);
-globalRouter.route("/qna").get(qna);
-globalRouter.route("/qna/:id(\\d+)").get(seeQ);
-globalRouter.route("/uploadq").get(getUploadQ).post(postUploadQ);
-globalRouter.route("/editq").get(getEditQ).post(postEditQ);
 
 export default globalRouter;
---
+++ src/routers/qnaRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { qna, seeQ, getUploadQ, postUploadQ, getEditQ, postEditQ } from "../controllers/qnaController";
+
+const globalRouter = express.Router();
+
+globalRouter.route("/").get(qna);
+globalRouter.route("/:id(\\d+)").get(seeQ);
+globalRouter.route("/uploadq").get(getUploadQ).post(postUploadQ);
+globalRouter.route("/editq").get(getEditQ).post(postEditQ);
