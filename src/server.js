6c5325587921967efd6490f9d6b2879cc6e6a68d
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-08-29 11:17:05 +0900

    08.29.morning mongo store resave save unonitialized

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 
 import rootRouter from "./routers/rootRouter";
 import lockerRouter from "./routers/lockerRouter";
@@ -52,8 +53,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
       secret: "Hello!",
-      resave: true,
-      saveUninitialized: true,
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/locker" }),
     })
 );
 
