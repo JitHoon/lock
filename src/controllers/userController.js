245316d05537bafbb22bf61aac7726ba315925b6
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-09-05 19:23:49 +0900

    09.05.night remove fetch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
-import fetch from "node-fetch";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
