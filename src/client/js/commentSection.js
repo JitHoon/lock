2a0eece12ff381859711c9d00f6b127848dc1c0f
Son Jung Won <102501352+songarden@users.noreply.github.com>
2022-10-10 01:37:54 +0900

    댓글 DB연결 + 프론트엔드로 댓글 보이기 구현 + 실시간댓글 업로드 -> 부하가 걸려서 변경할 예정

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -2,21 +2,24 @@ const questionContainer = document.getElementById("questionContainer");
 const form = document.getElementById("commentForm");
 const textarea = form.querySelector("textarea");
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
     event.preventDefault();
     const text =textarea.value;
     const questionId = questionContainer.dataset.id;
     if(text === ""){
-        return;
+        return; //댓글이 공백일때 전송 x
     }
-    fetch(`/qna/${questionId}/comment`,{
+    await fetch(`/qna/${questionId}/comment`,{
         method:"POST",
         headers: {
             "Content-Type": "application/json",  //string을 보내주지만 json string이라고 알려주는 표시
         },
         body: JSON.stringify({text}), //req.body 속 내용을 전달하는 역할을 함 
     });
+    textarea.value = "";
+    window.location.reload(); //자동 새로고침 ! -> 부하걸리는 작업이라 변환할 거임
 };
  
-form.addEventListener("submit ", handleSubmit);
+form.addEventListener("submit", handleSubmit);
+
 
