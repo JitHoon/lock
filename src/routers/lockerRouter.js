bfd8dad5e92b42610377c27e1ec7ae18b6bbfb41
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-11-06 13:47:37 +0900

    Locker source code 9. add /locker/_id lockerRouter

--- src/routers/lockerRouter.js
+++ src/routers/lockerRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { mainLocker, alphabetLocker,
-    getAplly, postAplly, } from "../controllers/lockerController";
+import { mainLocker,
+    getSignup, postSignup, } from "../controllers/lockerController";
 import { publicOnlyMiddleware, protectorMiddleware } from "../middlewares";
 
 const lockerRouter = express.Router();
@@ -10,13 +10,9 @@ lockerRouter
 .all(protectorMiddleware)
 .get(mainLocker);
 lockerRouter
-.route("/alphabet") // 3*3 사물함 보여줌
+.route("/:id([0-9a-f]{24})") // 사물함을 하나 클릭했을 때
 .all(protectorMiddleware)
-.get(alphabetLocker)
-lockerRouter
-.route("/alphabet/:id([0-9a-f]{24})") // 사물함을 하나 클릭했을 때
-.all(protectorMiddleware)
-.get(getAplly)
-.post(postAplly); // 버튼 구현
+.get(getSignup)
+.post(postSignup); // 버튼 구현
 
 export default lockerRouter;
