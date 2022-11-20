73e310431132ff19117718c0a9bf531e09dadcb7
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-11-20 21:26:32 +0900

    edit db connect

--- src/db.js
+++ src/db.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/locker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://127.0.0.1:27017/locker");
 
 const db = mongoose.connection;
 
