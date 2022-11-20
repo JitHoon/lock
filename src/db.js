9a2732604f46041e812b2980682c55ef463e12c5
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-11-20 22:59:20 +0900

    edit db.js connect

--- src/db.js
+++ src/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
   useFindAndModify: false,
-  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
