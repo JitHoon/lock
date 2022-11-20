a9aa2d36148d3375bb5b488357e9e31629d519c9
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-11-20 21:01:49 +0900

    edit package.json engines, PORT

--- build/init.js
+++ build/init.js
@@ -11,7 +11,7 @@ require("./models/Admin");
 require("./models/Record");
 var _server = _interopRequireDefault(require("./server"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var PORT = 4000;
+var PORT = process.env.PORT || 4000;
 var handleListening = function handleListening() {
   return console.log("Server listening on port http://localhost:".concat(PORT, " \uD83E\uDEE1"));
 };
